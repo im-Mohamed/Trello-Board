f73a5b5a5260c84c27079ee6159e30144eff7237
MohamedHassan3494 <muhamed.hassan3494@gmail.com>
2022-11-15 20:27:09 +0200

    Changing Js File

--- JS/Custom.js
+++ JS/Custom.js
@@ -1,41 +1,16 @@
-/* 
- show / hide button 
----------------------
-*/
-// create variables
+var toggleBtn = document.querySelector("#toggleBtn");
 
+var count = 0;
 
- 
-const toggleBtn = document.querySelector("#toggleBtn");
-const divList = document.querySelector(".listHolder");
-
-
-// action to be taken when clicked on hide list button
-toggleBtn.addEventListener("click", hideButton);
-function hideButton() {
-  if (divList.style.display === "none") {
-    divList.style.display = "block";
-    toggleBtn.innerHTML = "Hide List";
-  } else {
-    divList.style.display = "none";
-    toggleBtn.innerHTML = "Show List";
-  }
-}
-
-/* 
- add list items
------------------
-*/
-// create variables
-const addInput = document.querySelector("#addInput");
-const addBtn = document.querySelector("#addBtn");
-
-function addLists() {
+function addLists(elementID, btnID, listHolder) {
+  var addInput = document.getElementById(elementID);
+  var addBtn = document.getElementById(btnID);
+  var divList = document.getElementById(listHolder);
   if (addInput.value === "") {
     alert("Enter the list name please!!!");
   } else {
-    const ul = divList.querySelector("ul");
-    const li = document.createElement("li");
+    var ul = divList.querySelector("ul");
+    var li = document.createElement("li");
     li.innerHTML = addInput.value;
     addInput.value = "";
     ul.appendChild(li);
@@ -43,18 +18,10 @@ function addLists() {
   }
 }
 
-// add list when clicked on add item button
-addBtn.addEventListener("click", btnAddList);
-function btnAddList() {
-  addLists();
-}
-
 // add list when pressed enter
-addInput.addEventListener("keyup", enterAdd);
-
-function enterAdd() {
+function enterAdd(elementID, btnID, listHolder) {
   if (event.which === 13) {
-    addLists();
+    addLists(elementID, btnID, listHolder);
   }
 }
 
@@ -63,22 +30,22 @@ function enterAdd() {
 ------------------------
 */
 // create variables
-const listUl = document.querySelector(".list");
-const lis = listUl.children;
+var listUl = document.querySelector(".list");
+var lis = listUl.children;
 
 function createBtn(li) {
   // create remove button
-  const remove = document.createElement("button");
+  var remove = document.createElement("button");
   remove.className = "btn-icon remove";
   li.appendChild(remove);
 
   // create down button
-  const down = document.createElement("button");
+  var down = document.createElement("button");
   down.className = "btn-icon down";
   li.appendChild(down);
 
   // create up button
-  const up = document.createElement("button");
+  var up = document.createElement("button");
   up.className = "btn-icon up";
   li.appendChild(up);
 
@@ -89,13 +56,11 @@ function createBtn(li) {
 for (var i = 0; i < lis.length; i++) {
   createBtn(lis[i]);
 }
-
 /* 
  enabling button actions (to move item up, down or delete)
 ------------------------------------------------------------
 */
-divList.addEventListener("click", upAndDown);
-function upAndDown(event) {
+function upAndDownAndDelete(event) {
   if (event.target.tagName === "BUTTON") {
     const button = event.target;
     const li = button.parentNode;
@@ -115,44 +80,63 @@ function upAndDown(event) {
     }
   }
 }
-
-/*
-Make HTML Structure With Name
-----------------------------------------
+/* 
+ enabling button actions (to delete remove it's Block )
+------------------------------------------------------------
 */
-const inpAddCard = document.getElementById("add-card-text");
-const btnAddCard = document.getElementById("add-card-button");
-const mainContainer = document.getElementById("container");
+
+const removeHTMLElement = function (e) {
+  e.parentElement.remove();
+};
+
+// action to be taken when clicked on hide list button
+var divList = document.getElementById("listHolder");
+function hideAndShow(elementID, btnID, listHolder) {
+  if (divList.style.display === "none") {
+    divList.style.display = "block";
+    toggleBtn.innerHTML = "Hide List";
+  } else {
+    divList.style.display = "none";
+    toggleBtn.innerHTML = "Show List";
+  }
+}
+
+// ===========================================================
+var inpAddCard = document.getElementById("add-card-text");
+var btnAddCard = document.getElementById("add-card-button");
+var mainContainer = document.getElementById("container");
 
 btnAddCard.addEventListener("click", htmlGreat);
 function htmlGreat() {
+  count++;
   if (inpAddCard.value === "") {
     alert("Enter The Name please...");
   } else if (inpAddCard.value != "") {
     mainContainer.innerHTML += `
     <div class="todoBlock">
+    <button id="del-all${count}" class="removeAll" onclick="removeHTMLElement(this);">X</button>
       <!-- main title holder -->
       <div class="titleHolder">
         <h1>${inpAddCard.value}</h1>
-        <button id="del-all">X</button>
+        
       </div>
       <!-- todo list -->
       <div class="todoList">
         <!-- list holder -->
-        <div class="listHolder">
+        <div id="listHolder${count}" onclick="upAndDownAndDelete(event)">
           <ul class="list">
           </ul>
         </div>
         <!-- form holder -->
         <div class="formHolder">
           <div class="col big">
-            <input type="text" id="addInput" placeholder="Add Task...">
+            <input type="text" id="addInput${count}" onkeyup="enterAdd('addInput${count}','addBtn${count}','listHolder${count}')"  placeholder="Add Task...">
           </div>
           <div class="col">
-            <button type="button" id="addBtn" class="btn-primary">+</button>
+            <button type="button" id="addBtn${count}" onclick="addLists('addInput${count}','addBtn${count}','listHolder${count}')" class="btn-primary">+</button>
           </div>
           <div class="col">
-            <button type="button" id="toggleBtn" class="btn-primary">Hide List</button>
+            <button type="button" id="toggleBtn${count}"  class="btn-primary">Hide List</button>
           </div>
         </div>
       </div>
